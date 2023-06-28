f7e0e21a2651ecf0e17fae7d17dc3b1f7f750b78
Thomas Ekhardt <thomas.ekhardt@sidion.de>
2023-06-28 16:33:12 +0200

    faf

---
+++ 6. Vorlesung/src/script.js
@@ -0,0 +1,33 @@
+const { Octokit } = require("@octokit/rest");
+
+const message = document.getElementById('message');
+const list = document.getElementById('repos-list');
+const octokit = new Octokit();
+
+async function getRepos() {
+  list.innerHTML = '';
+  message.innerHTML = '';
+
+  try {
+    const response = await octokit.request('GET /user/repos', {
+      headers: {
+        authorization: `Bearer ${password}`
+      }
+    });
+
+    if (response.status === 200) {
+      const data = response.data;
+      data.forEach(element => {
+        const listElement = document.createElement('li');
+        const elementName = document.createTextNode(element.name);
+        listElement.appendChild(elementName);
+        list.appendChild(listElement);
+      });
+    } else {
+      throw new Error(response);
+    }
+  } catch (error) {
+    const err = await error.json();
+    message.innerHTML = err.message;
+  }
+}
